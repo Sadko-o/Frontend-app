71f60b30b0b944be2ae4e84318197cad0bfba12f
Sadko-o <r.saduakhas@gmail.com>
2022-11-23 14:38:59 +0600

    commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import './App.css';
 import Main from './components/Main';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Tables from './components/Tables';
 import DiseaseType from './components/DiseaseType';
 import Country from './components/Country';
 import Disease from './components/Disease';
@@ -17,22 +16,17 @@ function App() {
   return (
       
       <Router>
-        {/* {/* <Route exact path='/' element={< Main />}></Route> */}
-        {/* { Route path!= '/'? (<Tables/>) : null} */} 
         <Routes>
-          
             <Route exact path='/' element={< Main />}></Route>
-            <Route exact path='/tables' element={< Tables />}></Route>
             <Route exact path='/diseaseType' element={< DiseaseType />}></Route>
             <Route exact path='/country' element={< Country />}></Route>
             <Route exact path='/disease' element={< Disease />}></Route>
             <Route exact path='/discover' element={< Discover />}></Route>
             <Route exact path='/users' element={< Users />}></Route>
             <Route exact path='/publicServant' element={< PublicServant />}></Route>
-            <Route exact path='/doctor' element={< Doctor />}></Route>
+            <Route exact path='/doctors' element={< Doctor />}></Route>
             <Route exact path='/specialize' element={< Specialize />}></Route>
             <Route exact path='/record' element={< Record />}></Route>
-            {/* <Route exact path='/contact' element={< Contact />}></Route> */}
         </Routes>
       </Router>
     // </div>
